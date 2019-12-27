a633cad90b4f4160c41f16dbc2607deacd00957e
Nick Mandel <nwmandel@gmail.com>
2019-12-26 16:16:04 -0800

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# rust-learning-ex
+- rust book tutorial with installation `https://doc.rust-lang.org/book/ch01-01-installation.html`
+- links to various tutorials `https://github.com/ctjhoa/rust-learning`
+- online rust by example for small chapters `https://doc.rust-lang.org/stable/rust-by-example/index.html`
---
+++ variables/src/main.rs
@@ -0,0 +1,63 @@
+
+// struct User {
+//     username: String,
+//     email: String,
+//     sign_in_count: u64,
+//     active: bool,
+// }
+
+// fn main() {
+//     let mut user1 = User {
+//         email: String::from("someone@example.com"),
+//         username: String::from("someusername123"),
+//         active: true,
+//         sign_in_count: 1,
+//     };
+
+//     user1.email = String::from("anotheremail@exmaple.com");
+// }
+
+// fn bind_user(email: String, username: String) -> User {
+//     User {
+//         email,
+//         username,
+//         active: true,
+//         sign_in_count: 1,
+//     }
+// }
+
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    generate_workout(26,2);
+}
+
+fn generate_workout(intensity: u32, random_number: u32) {
+    let expensive_closure = |num| {
+        println!("calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    };
+
+    if intensity < 25 {
+        println!(
+            "Today, do {} pushups!",
+            expensive_closure(intensity)
+        );
+        println!(
+            "Next, do {} situps!",
+            expensive_closure(intensity)
+        );
+    } else {
+        if random_number == 3 {
+            println!("Take a break today! Remember to stay hydrated!");
+        } else {
+            println!(
+                "Today, run for {} minutes!",
+                expensive_closure(intensity)
+            );
+        }
+    }
+}
+
